bdd64e8f26acf3b2edf692497e3353ef489f2bd2
Newtanmukh <190010027@iitdh.ac.in>
2022-04-28 23:23:44 +0530

    complete it tomorrow

--- File Organizer/main.js
+++ File Organizer/main.js
@@ -6,6 +6,14 @@ let fs=require("fs"); //a function called sfexistssync will let you know if a pa
 let path=require("path"); // PATH JOIN, to create folders.
 
 
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+
+
 let command=inputArr[0];
 
 switch(command){
@@ -29,6 +37,7 @@ function treeFn(dirPath){
 
 function organizeFn(dirPath){
     //  1. input : directory path given.
+    let destPath;
     if(dirPath==undefined){
         console.log("kindly enter the path.");
         return;
@@ -36,8 +45,13 @@ function organizeFn(dirPath){
         let doesExist=fs.existsSync(dirPath); // WILL CHECK IF THIS PARTICULAR DIRECTORY PATH IS VALID OR NOT.
         if(doesExist){//START ORGANIZING.
                     //  2. create a directory called organized_files inside that directory path.
-            let destPath =  path.join(dirPath,"organized_files"); //create this organized files directory here.
-             fs.mkdirSync(destPath);
+            destPath =  path.join(dirPath,"organized_files"); //create this organized files directory here.
+            
+            if(fs.existsSync(destPath)==false){
+            fs.mkdirSync(destPath);//but if that directory already exists, then it creating that folder again without checking if it already exists, would have given error.
+
+            }
+
 
         }else{
             console.log("Kindly enter a correct path");
@@ -45,11 +59,69 @@ function organizeFn(dirPath){
         }
     }
 
+    organizeHelper(dirPath,destPath);
 
-    //3. check all files to check which category they belong to of the ones present in that path.
-    //4. copy/cut files to that organized directory inside of any of that category folder.
+
+    
+    
     //
 }
+
+
+function organizeHelper(src,dest){
+//3. check all files to check which category they belong to of the ones present in that path.
+   let childNames = fs.readdirSync(src) //will give the names of all the files indide this.
+   for(let i=0;i<childNames.length;i++)
+   {
+       let childAddress=path.join(src,childNames[i]);
+      let isFile= fs.lstatSync(childAddress).isFile();//checking if it is a file or not.
+       if(isFile){
+         
+          let category=getCategory(childNames[i]);
+             console.log(childAddress[i],"belongs to --> ",category);
+
+               //4. copy/cut files to that organized directory inside of any of that category folder.
+               sendFile(childAddress,dest,category);
+
+
+       }
+
+   }
+
+
+}
+
+function sendFile(srcFile,dest,category){
+
+    let categoryPath = path.join(dest, category);
+    if (fs.existsSync(categoryPath) == false) {
+        fs.mkdirSync(categoryPath);
+    }
+    let fileName = path.basename(srcFilePath);
+    let destFilePath = path.join(categoryPath, fileName);
+    fs.copyFileSync(srcFilePath, destFilePath);
+    fs.unlinkSync(srcFilePath);
+    console.log(fileName, "copied to ", category);
+}
+
+
+function getCategory(name){
+    let ext = path.extname(name);//get the extension.
+    ext = ext.slice(1);//remove the dot
+    for (let type in types) { 
+        let cTypeArray = types[type];
+        for (let i = 0; i < cTypeArray.length; i++) {
+            if (ext == cTypeArray[i]) {
+                return type;
+            }
+        }
+    }
+    return "others";
+
+}
+
+
+
 
 function helpFn(){
     console.log(`List of all the commands:
